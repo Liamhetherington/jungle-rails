2f1a1733a5ae50c10b1c284707dafc6326e97507
Liam Hetherington <liambhetherington@gmail.com>
2019-06-14 19:32:55 +0000

    create ratings feature for products. add ratings to seed and reset db

---
+++ app/models/rating.rb
@@ -0,0 +1,6 @@
+class Rating < ActiveRecord::Base
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: {only_integer: true, greater_than: -1, less_than: 6}
+end
